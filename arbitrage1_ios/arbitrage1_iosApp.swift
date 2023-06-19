77afe722cc8faf3ba0c5e7e89cfacac669066e3a
Nizam Mamedov <nizammamedovv@gmail.com>
2023-06-19 13:57:51 +0200

    Initial Commit

---
+++ arbitrage1_ios/arbitrage1_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  arbitrage1_iosApp.swift
+//  arbitrage1_ios
+//
+//  Created by Nizam Mamedzadeh on 19.06.23.
+//
+
+import SwiftUI
+
+@main
+struct arbitrage1_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
